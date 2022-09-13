7d247444dd74cf13a43538401eb8c35af8de42e5
Cadence Data Soft <banand@cadencedatasoft.in>
2022-09-13 17:49:21 +0530

    This script is not non interactive

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
+sudo apt-get -y install lsb-release ca-certificates apt-transport-https software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
 sudo systemctl status --no-pager docker
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
